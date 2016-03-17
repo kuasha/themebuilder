e8ada94d01a8d0592902593ec50065438814bbb3
Maruf Maniruzzaman <kuasha@gmail.com>
2016-03-17 09:33:52 -0700

    Added no-cache to web server

--- webserver.py
+++ webserver.py
@@ -1,11 +1,20 @@
 import SimpleHTTPServer
 import SocketServer
 
+import SimpleHTTPServer
+
+class NoCacheHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+
 PORT = 8000
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+Handler = NoCacheHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-print "serving at port", PORT
+print "Serving at port", PORT
 httpd.serve_forever()
